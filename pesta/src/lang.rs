207074ef0dcbee0ac62574893715956d6b9d7c75
ksqsf <i@ksqsf.moe>
2019-10-28 08:26:25 +0800

    [pesta] use json to workaround emacs limitation
    emacs doesn't support send multiple EOFs

--- pesta/src/lang.rs
+++ pesta/src/lang.rs
@@ -4,19 +4,26 @@ use pest_meta::{parser, optimizer, validator};
 use pest_meta::parser::Rule;
 use std::io;
 use std::io::Read;
+use json::{self, JsonValue};
 
 pub fn check(rule: &str) {
     let stdin = io::stdin();
     let mut handle = stdin.lock();
-    let mut meta_buf = String::new();
-    handle.read_to_string(&mut meta_buf).expect("meta read stdin");;
+    let mut buffer = String::new();
+    handle.read_to_string(&mut buffer).expect("read stdin");;
+    let json = json::parse(&buffer).expect("json");
+    let (meta_buf, input_buf) = match json {
+        JsonValue::Array(list) => {
+            (list[0].as_str().map(|s| s.to_owned()).expect("grammar"),
+             list[1].as_str().map(|s| s.to_owned()).expect("input"))
+        }
+        _ => panic!("malformed json input"),
+    };
     let meta = parser::parse(Rule::grammar_rules, &meta_buf).expect("parse meta");
     validator::validate_pairs(meta.clone()).expect("meta pairs validation");
     let ast = parser::consume_rules(meta).expect("meta to ast");
     let vm = Vm::new(optimizer::optimize(ast.clone()));
 
-    let mut input_buf = String::new();
-    handle.read_to_string(&mut input_buf).expect("input read stdin");;
     match vm.parse(rule, &input_buf) {
         Ok(_) => {},
         Err(error) => {
